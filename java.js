0c5ee418c57c3e4287ab49fc245da0e7e5c22408
Juan Angel Loaeza Carrillo <152027089+JuanAngelLoaezaCarrillo@users.noreply.github.com>
2024-01-08 14:12:03 -0600

    SUDOKU
    SODUKU

--- java.js
+++ java.js
@@ -1,37 +1,47 @@
-document.getElementById("botonIniciar").addEventListener("click", function () {
-    let resultado = campeonato();
-    document.getElementById("resultado").innerHTML = `El campeon es: ${resultado}`;
-   });
+function ElSudoku(grid) {
+    const regionSize = 3;
+    const gridSize = 9;
    
-   function campeonato() {
-    let equipos = [
-       "Argentina",
-       "Holanda",
-       "Croacia",
-       "Brasil",
-       "Marruecos",
-       "Portugal",
-       "Francia",
-       "Inglaterra",
-    ];
+    for (let row = 0; row < gridSize; row++) {
+        const rowSet = new Set();
+        const columnSet = new Set();
+        const regionSet = new Set();
    
-    let partidos = [
-       [1, 2], [3, 4], [5, 6], [7, 8],
-       [1, 3], [2, 4], [5, 7], [6, 8],
-       [1, 5], [2, 6], [3, 7], [4, 8]
-    ];
+        for (let column = 0; column < gridSize; column++) {
+          const cellValue = grid[row][column];
    
-    let alParecerDe = equipos[0];
-    for (let i = 0; i < partidos.length; i++) {
-       let partido = partidos[i];
-       let equipoA = equipos[partido[0] - 1];
-       let equipoB = equipos[partido[1] - 1];
+          if (rowSet.has(cellValue) || columnSet.has(cellValue)) {
+            return "falso";
+          }
    
-       let ganador = Math.argentina() > 0.5 ? equipoA : equipoB;
-       if (ganador === alParecerDe) {
-         alParecerDe = ganador;
-       }
+          rowSet.add(cellValue);
+          columnSet.add(cellValue);
+   
+          const regionRow = Math.floor(row / regionSize);
+          const regionColumn = Math.floor(column / regionSize);
+          const regionIndex = regionRow * regionSize + regionColumn;
+          const regionCellValue = regionSet.has(cellValue);
+   
+          if (regionCellValue) {
+            return "falso";
+          }
+   
+          regionSet.add(cellValue);
+        }
     }
    
-    return alParecerDe;
+    return "verdadero";
    }
+   
+   const grid = [
+    [1, 8, 9, 4, 2, 5, 3, 6, 7],
+    [6, 7, 5, 9, 1, 3, 2, 4, 8],
+    [3, 4, 2, 6, 7, 8, 1, 5, 9],
+    [2, 6, 7, 1, 4, 9, 8, 3, 5],
+    [9, 5, 3, 7, 8, 6, 4, 2, 1],
+    [4, 1, 8, 5, 3, 2, 9, 7, 6],
+    [8, 9, 4, 2, 5, 7, 6, 1, 3],
+    [5, 3, 1, 8, 6, 4, 7, 9, 2],
+    [7, 2, 6, 3, 9, 1, 5, 8, 4]];
+   
+   console.log(ElSudoku(grid)); // Output: verdadero
